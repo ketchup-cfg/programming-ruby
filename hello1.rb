1196394f7b0061a849443ad13b5b6d2b8c159c2e
Trevor Pierce <yeahtrevorpierce@gmail.com>
2025-11-30 16:04:43 -0600

    Add Gemfile and rubocop

---
+++ hello1.rb
@@ -0,0 +1,8 @@
+def say_hello_goodbye(name)
+  result = "I don't know why you say goodbye, " + name + ", I say hello"
+  return result
+end
+
+# call the method
+puts say_hello_goodbye("John")
+puts say_hello_goodbye("Paul")
